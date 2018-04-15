d12f3cb8ca43f2b0813221454b5af29cca1cfecf
Jawann <jawann@gmail.com>
2018-04-15 02:12:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ module FormsLab
       #@pirate_name = params[pirate["name"]]
       #@ship_name_1 = params[pirate[ships][0]["name"]]
       #@ship_name_2 = params[pirate[ships][1]["name"]]
-      @pirate_name = pirate[:name]
+      #@pirate_name = pirate[:name]
       @ship_1 = pirate[:ships][0]
       @ship_2 = pirate[:ships][1]
       erb :'pirates/show'
